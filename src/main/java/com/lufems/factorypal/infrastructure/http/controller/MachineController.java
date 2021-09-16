4ef39b3df5dcb6293737de31bbc328d754643fe5
Luiz Souza <luizfelipematoss@gmail.com>
2021-09-16 22:51:47 +0100

    Adding parameter endpoints into MachineController

--- src/main/java/com/lufems/factorypal/infrastructure/http/controller/MachineController.java
+++ src/main/java/com/lufems/factorypal/infrastructure/http/controller/MachineController.java
@@ -1,10 +1,14 @@
 package com.lufems.factorypal.infrastructure.http.controller;
 
 import com.lufems.factorypal.domain.model.Machine;
+import com.lufems.factorypal.domain.model.Parameter;
 import com.lufems.factorypal.domain.service.MachineService;
+import com.lufems.factorypal.domain.service.ParameterService;
 import com.lufems.factorypal.infrastructure.http.controller.model.MachineRest;
 import com.lufems.factorypal.infrastructure.http.controller.model.Request.NewMachineRequest;
+import com.lufems.factorypal.infrastructure.http.controller.model.Request.NewParametersRequest;
 import com.lufems.factorypal.infrastructure.mapper.MachineMapper;
+import com.lufems.factorypal.infrastructure.mapper.ParameterMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,20 +22,24 @@ import java.util.stream.Collectors;
 @RequestMapping("/machines")
 public class MachineController {
 
-    private MachineService service;
-    private MachineMapper mapper;
+    private MachineService machineService;
+    private MachineMapper machineMapper;
+    private ParameterMapper parameterMapper;
+    private ParameterService parameterService;
 
     @Autowired
-    MachineController(MachineService service, MachineMapper mapper) {
-        this.service = service;
-        this.mapper = mapper;
+    MachineController(MachineService machineService, MachineMapper machineMapper, ParameterMapper parameterMapper, ParameterService parameterService) {
+        this.machineService = machineService;
+        this.machineMapper = machineMapper;
+        this.parameterMapper = parameterMapper;
+        this.parameterService = parameterService;
     }
 
     @GetMapping("")
-    public ResponseEntity<List<MachineRest>> list() {
-        List<MachineRest> response = service.listAllMachines()
+    public ResponseEntity<List<MachineRest>> listAllMachines() {
+        List<MachineRest> response = machineService.listAllMachines()
                  .stream()
-                .map(this.mapper::domainToRest)
+                .map(this.machineMapper::domainToRest)
                 .collect(Collectors.toList());
 
         return ResponseEntity.ok(response);
@@ -39,14 +47,14 @@ public class MachineController {
 
     @GetMapping("/{machineKey}")
     public ResponseEntity<MachineRest> findMachine(@PathVariable(name = "machineKey") String key) {
-        MachineRest response = mapper.domainToRest(service.findMachine(key));
+        MachineRest response = machineMapper.domainToRest(machineService.findMachine(key));
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("")
     public ResponseEntity<MachineRest> addMachine(@RequestBody NewMachineRequest request) {
-        Machine newMachine = mapper.newMachineRequestToDomain(request);
-        service.save(newMachine);
+        Machine newMachine = machineMapper.newMachineRequestToDomain(request);
+        machineService.save(newMachine);
 
         String baseUrl = ServletUriComponentsBuilder
                 .fromCurrentContextPath()
@@ -54,6 +62,22 @@ public class MachineController {
                 .toUriString();
 
         String path = "/machines/" + newMachine.getKey();
+
+        return ResponseEntity.created(URI.create(baseUrl + path)).build();
+    }
+
+    @PostMapping("/parameters")
+    public ResponseEntity<URI> addParameters(@RequestBody NewParametersRequest newParams) {
+
+        List<Parameter> params = this.parameterMapper.requestToDomain(newParams);
+
+        this.parameterService.saveAll(params);
+
+        final String path = "/machines/" + newParams.getMachineKey();
+        final String baseUrl = ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .build()
+                .toUriString();
 
         return ResponseEntity.created(URI.create(baseUrl + path)).build();
     }
